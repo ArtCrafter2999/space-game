99023286ed9a8d3035afbde3c714271ffc7a3fff
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2025-06-05 21:48:38 +0300

    fix camera in physics process

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,7 +10,8 @@ class_name Player
 @export var jump_velocity: float = 4.5
 
 @onready var neck: Node3D = $Neck
-@onready var camera = $Neck/Camera3D;
+@onready var sub_neck: Node3D = $Neck/SubNeck
+@onready var camera = %Camera;
 @onready var on_floor_area: Area3D = $onFloorArea
 @onready var gravity_listener: GravityListener = $GravityListener
 
@@ -30,9 +31,11 @@ func handle_gravity_changed(gravity: Vector3):
 		#print("cross_product %s; angle %s" % [_cross_product, _cp_angle])
 
 var _esc: bool = true;
+var _neck_y_rotation = 0;
 var _y_rotation = 0;
 var gravity_length: float
-var head_rotation: float
+var head_x_rotation: float
+var body_x_rotation: float
 
 func _unhandled_input(event) -> void: 
 	if event is InputEventMouseButton && _esc:
@@ -45,8 +48,8 @@ func _unhandled_input(event) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and \
 	event is InputEventMouseMotion:
 		var motionEvent: InputEventMouseMotion = event
-		_y_rotation = -motionEvent.relative.x * 0.01 * sensativity
-		head_rotation = -motionEvent.relative.y * 0.01 * sensativity
+		_neck_y_rotation = -motionEvent.relative.x * 0.01 * sensativity
+		head_x_rotation = -motionEvent.relative.y * 0.01 * sensativity
 		
 
 func is_on_floor() -> bool: 
@@ -75,17 +78,17 @@ func _process(delta: float) -> void:
 	var gravity_limit = no_gravity || gravity_length > 18
 	
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		#if(_y_rotation):
-			#global_rotate(transform.basis.y, _y_rotation);
-			#_y_rotation=0;
+		if(_neck_y_rotation):
+			neck.rotate_y(_neck_y_rotation);
+			_y_rotation+=_neck_y_rotation
+			_neck_y_rotation=0;
 		
-		if(head_rotation):
+		if(head_x_rotation):
+			sub_neck.rotate_x(head_x_rotation)
+			sub_neck.rotation.x = clamp(sub_neck.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 			if(gravity_limit):
-				global_rotate(transform.basis.x, head_rotation);
-			else:
-				neck.rotate_x(head_rotation)
-				neck.rotation.x = clamp(neck.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-			head_rotation = 0
+				body_x_rotation += head_x_rotation
+			head_x_rotation = 0
 
 func _physics_process(delta):
 	gravity_length = gravity_listener.calculate_gravity().length()
@@ -97,11 +100,19 @@ func _physics_process(delta):
 	var gravity_limit = no_gravity || gravity_length > 18
 	rotate_to_gravity()
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		
-		# TODO: Треба пофіксити поворот, щоб він був в process
-		if(_y_rotation):
+		if(_neck_y_rotation):
 			global_rotate(transform.basis.y, _y_rotation);
-			_y_rotation=0;
+			neck.rotate_y(-_y_rotation);
+			_y_rotation = 0;
+		
+		if body_x_rotation:
+			if gravity_limit: 
+				global_rotate(transform.basis.x, sub_neck.rotation.x);
+				sub_neck.rotation.x = 0;
+			else:
+				global_rotate(transform.basis.x, body_x_rotation);
+				sub_neck.rotate_x(-body_x_rotation);
+			body_x_rotation = 0
 		
 		var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
 		var side = transform.basis.x * input_dir.x
